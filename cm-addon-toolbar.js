7dc1299a00988018c9fa68eec93957339c659c80
VSPRINTSEV <alterfan@outlook.com>
2019-04-06 05:57:46 +0300

    modified:   README.md 	modified:   cm-addon-toolbar.css 	modified:   cm-addon-toolbar.js

--- cm-addon-toolbar.js
+++ cm-addon-toolbar.js
@@ -7,9 +7,9 @@
 		mod(CodeMirror);
 })((CodeMirror) => {
 	'use strict';
-	let active = null,
+	let _cm = null,
 		panel, wrap;
-	let searchBar = $("<form>", {
+	/* let searchBar = $("<form>", {
 		"class": "cm-edit-toolbar_form",
 		append: $("<div>", {
 			"class": "group_search",
@@ -25,35 +25,84 @@
 				})
 			})
 		})
-	});
-	let editorBar = '<ul class="group group-max-width" data-action="search"><li class="group" data-action="search">' +
+	}); */
+	let editorBar = '<ul class="group group-max-width"><li class="group">' +
+		'<a class="group_btn group_btn-max-width" data-action="clear"><i class="material-icons">delete</i></a>' +
+		'</li><li class="group">' +
 		'<a class="group_btn group_btn-max-width" data-action="format"><i class="material-icons">code</i></a>' +
 		'</li><li class="group">' +
 		'<a class="group_btn group_btn-max-width" data-action="undo"><i class="material-icons">undo</i></a>' +
 		'</li><li class="group">' +
 		'<a class="group_btn group_btn-max-width" data-action="redo"><i class="material-icons">redo</i></a>' +
 		'</li></ul>';
-	var create = (cm, toolbar) => {
+	let actions = {
+		clear: function (cm) {
+			cm.setValue('');
+			cm.refresh()
+		},
+		undo: function (cm) {
+			cm.undo()
+			cm.refresh()
+		},
+		redo: function (cm) {
+			cm.redo();
+			cm.refresh();
+		},
+		format: function (cm) {
+			if (typeof cm.getOption('mode') == "object") {
+				var mode = (cm.getOption('mode')).name
+			} else {
+				var mode = cm.getOption('mode');
+			}
+			let code = cm.getValue();
+			if (mode == 'htmlmixed' || mode == 'html' || mode == 'xml') {
+				var formatted = html_beautify(code, {
+					'indent_size': 2,
+					'indent_char': '\t'
+				});
+			}
+			if (mode == 'javascript') {
+				var formatted = js_beautify(code, {
+					'indent_size': 2,
+					'indent_char': '\t'
+				});
+			}
+			if (mode == 'css') {
+				var formatted = css_beautify(code, {
+					'indent_size': 2,
+					'indent_char': '\t'
+				});
+			}
+			cm.setValue(formatted);
+			cm.setCursor(cm.getDoc().lineCount(), 0);
+		}
+	}
+	let action = new Function("action", "cm", "actions", "actions[action](cm);");
+	let create = (cm, toolbar) => {
 		wrap = cm.getWrapperElement();
-		panel = $('<div>', {
-			"id": "cm-edit-toolbar",
-			"class": "cm-edit-toolbar",
-			"style": "top:-50%;opacity: 0",
-			"html": toolbar
-		});
-		panel.insertBefore(wrap).animate({
-			opacity: 1,
-			top: "0%"
-		}, 250)
+		panel = document.createElement('div');
+		panel.className = 'cm-edit-toolbar';
+		wrap.parentNode.insertBefore(panel, wrap);
+		panel.innerHTML = toolbar;
 		cm.refresh()
+		let btns = panel.querySelectorAll('[data-action]');
+		for (var i = 0; i < btns.length; i++) {
+			btns[i].onclick = function () {
+				action(this.getAttribute('data-action'), cm, actions)
+			}
+		}
+	}
+	let show = (cm) => {
+		wrap = cm.getWrapperElement();
+		wrap.parentNode.querySelector(".cm-edit-toolbar").classList.add('visible');
+		cm.refresh();
 	}
 	let close = (cm) => {
-		panel.animate({
-			opacity: 0,
-			top: "-50%"
-		}, 200)
-		panel.remove()
-		cm.focus();
+		for (var i = 0; i < document.querySelectorAll(".cm-edit-toolbar").length; i++) {
+			document.querySelectorAll(".cm-edit-toolbar")[i].classList.remove('visible');
+			document.querySelectorAll(".cm-edit-toolbar")[i].remove()
+		}
+		cm.refresh();
 	}
 	CodeMirror.defineOption("toolBar", false, function (cm, val, old) {
 		if (old && old != CodeMirror.Init) {
@@ -62,13 +111,14 @@
 		if (old == CodeMirror.Init) old = false;
 		if (!old == !val) return;
 		if (val) {
-			CodeMirror.on(cm, "focus", (cm) => {
-				if (active != cm) {
+			CodeMirror.on(cm, "mousedown", (cm) => {
+				if (_cm != cm) {
 					if (panel) {
 						close(cm)
+						_cm = cm;
 					}
-					active = cm
-					return create(cm, editorBar)
+					create(cm, editorBar);
+					show(cm)
 				}
 			});
 		}
